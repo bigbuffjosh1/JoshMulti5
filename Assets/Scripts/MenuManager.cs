f3740ebf703af41cdc8a29d6007008fe7777ed21
Paul Sanchez <mr.jrmeatballs@gmail.com>
2025-11-29 23:34:09 -0800

    network 4

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,4 +1,6 @@
+using Edgegap;
 using Mirror;
+using Steamworks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +10,18 @@ public class MenuManager : MonoBehaviour
     public Button hostButton;
     public Button joinButton;
     public TMP_InputField addressField;
+
+    //Steam stuff
+    private Callback<LobbyCreated_t> randomGiberishVar1;
+    private Callback<LobbyEnter_t> randomGiberishVar2;
+
+
+    private void Awake()
+    {
+        randomGiberishVar1 = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        randomGiberishVar2 = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +33,11 @@ public class MenuManager : MonoBehaviour
     {
         //start host
         NetworkManager.singleton.StartHost();
-        Debug.Log("Hosting");
+        Debug.Log("Hosting w Mirror");
+
+        //start steam lobby
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, NetworkManager.singleton.maxConnections);
+        Debug.Log("Steam Lobby created (public)");
     }
 
     void JoinButtonPressed()
@@ -31,5 +49,36 @@ public class MenuManager : MonoBehaviour
             NetworkManager.singleton.StartClient();
             Debug.Log("Joining");
         }
+    }
+    
+    //host makes lobby
+    void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult == EResult.k_EResultOK)
+        {
+            CSteamID lobbyID = new CSteamID(callback.m_ulSteamIDLobby);
+
+            Debug.Log("Lobby created successfully! Lobby ID: " + callback.m_ulSteamIDLobby);
+            SteamMatchmaking.SetLobbyData(lobbyID, "HostName", SteamFriends.GetPersonaName());
+            SteamMatchmaking.SetLobbyData(lobbyID, "HostSteamID", SteamUser.GetSteamID().m_SteamID.ToString());
+        }
+        else
+        {
+            Debug.LogError("Failed to make lobby : " + callback.m_eResult);
+        }
+    }
+
+    //client joins lobby
+    void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        CSteamID lobbyID = new CSteamID(callback.m_ulSteamIDLobby);
+
+        // Read the host SteamID stored in the lobby metadata
+        string hostIDString = SteamMatchmaking.GetLobbyData(lobbyID, "HostSteamID");
+
+        //Actually join
+        NetworkManager.singleton.networkAddress = hostIDString;
+        NetworkManager.singleton.StartClient();
+        Debug.Log("Joining host via Steam invite: " + hostIDString);
     }
 }
--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -7,26 +7,47 @@ public class PlayerSetup : NetworkBehaviour
     public GameObject cameraPrefab;
     public GameObject playerHead;
 
-    private GameObject sceneCamera;
     private CinemachineCamera cinemaCam;
     private GameObject menuCanvas;
     public override void OnStartLocalPlayer()
     {
-        //Handle the camera
+        //Spawn in the camera prefab here!!
         GameObject cam = Instantiate(cameraPrefab);
 
-        sceneCamera = GameObject.Find("SceneCamera");
-        sceneCamera.SetActive(false);
+        CameraFollowPlayer(cam);
 
+        //Hide the menu canvas
+        HideMenuCanvas();
+        //Also Hide Cursor
+        HideCursor();
+        //Hide the first camera in the scene
+        HideSceneCamera();
+    }
+
+    void CameraFollowPlayer(GameObject cam)
+    {
         cinemaCam = cam.GetComponentInChildren<CinemachineCamera>();
         cinemaCam.Follow = playerHead.transform;
 
         //Assign Cam to Follow player's head in Player movement
         GetComponent<PlayerMovement>().cam = cinemaCam.transform;
+    }
 
-        //Hide the menu canvas
+    void HideSceneCamera()
+    {
+        GameObject sceneCamera = GameObject.Find("SceneCamera");
+        sceneCamera.SetActive(false);
+    }
+
+    void HideCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void HideMenuCanvas()
+    {
         menuCanvas = GameObject.Find("Canvas");
         menuCanvas.SetActive(false);
-
     }
 }
